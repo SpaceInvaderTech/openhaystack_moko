e4be6b0bc8922bb3b271011ca4c08f34e9a9d308
Antonio Calatrava <antonio@antoniocalatrava.com>
2026-06-10 11:32:43 +0200

    change TAG name to SpInv and added full MAC to advertising name

--- apps/firmware/src/ble_stack.c
+++ apps/firmware/src/ble_stack.c
@@ -37,7 +37,7 @@
 #define SLAVE_LATENCY                   0                                           /**< Slave latency. */
 #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds). */
 
-#define DEVICE_NAME                     "TAG"                                       /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME                     "SpInv"                                       /**< Name of device. Will be included in the advertising data. */
 #define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000)                       /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000)                      /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
@@ -480,8 +480,11 @@ void gap_params_init(void)
     memset(&gap_addr, 0, sizeof(gap_addr));
     err_code = sd_ble_gap_addr_get(&gap_addr);
 
-    char name[strlen(DEVICE_NAME) + 7];
-    sprintf(name, "%s_%02X%02X", DEVICE_NAME, gap_addr.addr[1], gap_addr.addr[0]);
+    char name[strlen(DEVICE_NAME) + 13];
+    sprintf(name, "%s%02X%02X%02X%02X%02X%02X",
+            DEVICE_NAME,
+            gap_addr.addr[5], gap_addr.addr[4], gap_addr.addr[3],
+            gap_addr.addr[2], gap_addr.addr[1], gap_addr.addr[0]);
 
     err_code = sd_ble_gap_device_name_set(&sec_mode,
                                           (const uint8_t *)name,
